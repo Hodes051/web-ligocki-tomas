e8414ebc5ae0429ff655b193a19bd1bed475d1b3
Hodes051 <134860033+Hodes051@users.noreply.github.com>
2024-11-11 09:19:52 +0100

    Delete FullVersion directory

--- FullVersion/InsuranceEvidence/src/main/java/evidence/data/repositories/InsuredRepository.java
+++
@@ -1,7 +0,0 @@
-package evidence.data.repositories;
-
-import evidence.data.entities.InsuredEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InsuredRepository extends JpaRepository<InsuredEntity, Long> {
-}
