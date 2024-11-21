eb2550e302792f9760dc8332e7e01eb4ee51fa81
ligocki <ligockit@seznam.cz>
2024-11-22 00:01:14 +0100

    update 0.2

--- FullVersion/target/generated-sources/annotations/evidence/models/dtos/mappers/InsuredMapperImpl.java
+++ FullVersion/target/generated-sources/annotations/evidence/models/dtos/mappers/InsuredMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-17T21:56:36+0100",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 21.0.3 (Amazon.com Inc.)"
+    date = "2024-11-21T23:42:22+0100",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 21.0.3 (Eclipse Adoptium)"
 )
 @Component
 public class InsuredMapperImpl implements InsuredMapper {
